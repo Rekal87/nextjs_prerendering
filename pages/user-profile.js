3cbb45ea3669c6f0478bc6be1e7232c6fd038dd4
Nnamdi <nnamdi.b@gmail.com>
2022-07-13 00:41:29 +0200

    moved Products into a folder, changed some code

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -5,8 +5,7 @@ export default function UserProfilePage(props) {
 export async function getServerSideProps(context) {
   const { params, req, res } = context;
 
-  console.log(req);
-  console.log(res);
+  console.log('Server Side Code');
 
   return {
     props: {
