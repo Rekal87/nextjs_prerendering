7c0f210cefeb1dc02a67a636234103ca7dc7e493
Nnamdi <nnamdi.b@gmail.com>
2022-07-12 22:40:18 +0200

    getStaticProps & configuration

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   // constructing a path for the ReadFile to read file
   // cwd = current working directory
   /* process.cwd = root directory, then you add in arguments for where you wanna go,
@@ -24,6 +24,22 @@ export async function getStaticProps() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  // if no data existed and you wanted to redirect the user to a different page
+  if (!data) {
+    return {
+      redirect: {
+        destnation: '/no-data',
+      },
+    };
+  }
+
+  // if you fail to fetch data, you'll get the 404 not found page
+  if (data.product.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
